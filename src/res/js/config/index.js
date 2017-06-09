a9e1a89de062000009b443edd72a31e762ec148b
Vinsea <smallheidou@qq.com>
2017-06-09 15:31:29 +0800

    [-]去掉无用的vue

--- src/res/js/config/index.js
+++ src/res/js/config/index.js
@@ -1,11 +1,15 @@
 /**
  * Author:Vinsea.
  * CreateDate:2017/6/9 14:05.
- * Describe:接口地址配置
+ * Describe:系统配置
  */
 const BASE_URL = '/uubian'
 export default {
+  /** 接口地址配置 */
   baseUrl: BASE_URL,
   baseApiUrl: BASE_URL + '/web/api',
   baseCommentUrl: BASE_URL + '/comment/api',
+
+  /** 是否是发布状态，不是的话会显示网站上线倒计时 */
+  isPublish:true
 }
