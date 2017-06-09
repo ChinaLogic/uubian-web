15919bc82af1071117e03d70579a590248d1c9cf
Vinsea <smallheidou@qq.com>
2017-06-09 14:23:42 +0800

    [+]常量、工具类等公用部分提取出来mixin到Vue

---
+++ src/res/js/common/consts.js
@@ -0,0 +1,11 @@
+/**
+ * Author:Vinsea.
+ * CreateDate:2017/6/9 14:09.
+ * Describe:常量库
+ */
+export default {
+  /** 分页相关 */
+  page: {
+    pageSize: 10
+  }
+}
---
+++ src/res/js/config/index.js
@@ -0,0 +1,11 @@
+/**
+ * Author:Vinsea.
+ * CreateDate:2017/6/9 14:05.
+ * Describe:接口地址配置
+ */
+const BASE_URL = '/uubian'
+export default {
+  baseUrl: BASE_URL,
+  baseApiUrl: BASE_URL + '/web/api',
+  baseCommentUrl: BASE_URL + '/comment/api',
+}
